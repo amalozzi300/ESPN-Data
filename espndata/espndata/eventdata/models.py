1ab4eb9a11e184e7dc3f5290bc472ea5c0cc7099
amalozzi300 <andrewmalozzi900@gmail.com>
2025-09-17 16:13:27 -0400

    event model

--- espndata/espndata/eventdata/models.py
+++ espndata/espndata/eventdata/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    LEAGUE_CHOICES = [
+        ('college-football', 'NCAAF'),
+        ('nfl', 'NFL'),
+        ('nba', 'NBA'),
+        ('mlb', 'MLB'),
+    ]
+
+    league = models.CharField(max_length=16, choices=LEAGUE_CHOICES)
+    espn_id = models.CharField(max_length=24)
+    date = models.DateField()
+    season = models.IntegerField()
+    week = models.IntegerField(null=True)
+    home_team = models.CharField(max_length=128)
+    home_rank = models.IntegerField(null=True)
+    home_win_probability = models.DecimalField(max_digits=5, decimal_places=2)
+    is_home_win = models.BooleanField(default=False)
+    away_team = models.CharField(max_length=128)
+    away_rank = models.IntegerField(null=True)
+    away_win_probability = models.DecimalField(max_digits=5, decimal_places=2)
+    is_away_win = models.BooleanField(default=False)
+    is_neutral_site = models.BooleanField(default=False)
+    both_ranked_matchup = models.BooleanField(default=False)
+    one_ranked_matchup = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['league', 'espn_id'],
+                name='unique_league_event_id_combination',
+            )
+        ]
