5215fbb25b5c9bf5fc006c2d57840a1bd991f406
amalozzi300 <andrewmalozzi900@gmail.com>
2025-09-18 18:15:52 -0400

    fixed reverse logic in decimal_conversion util, framework and testing of summary_data command

--- espndata/espndata/utils/__init__.py
+++ espndata/espndata/utils/__init__.py
@@ -2,7 +2,9 @@ def convert_to_decimal(american):
     """ 
     Converts American format betting odds to decimal form.
     """
-    if american < 0:
+    if american is None:
+        return american
+    elif american > 0:
         return (american / 100) + 1
-    if american > 0:
+    elif american < 0:
         return 1 - (100 / american)
--- espndata/espndata/utils/management/commands/summary_data.py
+++ espndata/espndata/utils/management/commands/summary_data.py
@@ -6,6 +6,8 @@ import logging
 import requests
 import time
 
+from espndata.utils import convert_to_decimal
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 class Command(BaseCommand):
@@ -18,6 +20,12 @@ class Command(BaseCommand):
             'nba': 'basketball',
             'mlb': 'baseball',
         }
+        incomplete_event_data = {
+            'college-football': set(),
+            'nfl': set(),
+            'nba': set(),
+            'mlb': set(),
+        }
         base_espn_api_url = 'https://site.api.espn.com/apis/site/v2/sports/{sport}/{league}/summary'
         ids_file_path = settings.BASE_DIR / 'espndata' / '_raw_data' / 'all_event_ids.json'
         
@@ -27,11 +35,90 @@ class Command(BaseCommand):
         for league, ids_list in ids_by_league.items():
             sport = league_sport_map[league]
             formatted_url = base_espn_api_url.format(sport=sport, league=league)
-            logging.info(formatted_url)
-
+            
+            event_id = ids_list[671]
             
 
 
+            params = {'event': event_id}
+            event_summary = self.request_with_retry(formatted_url, params).json()
+
+            header_info = event_summary.get('header')
+            if not header_info:
+                incomplete_event_data[league].add(event_id)
+                continue
+            comp = next(iter(header_info.get('competitions', [])))
+            if not comp:
+                incomplete_event_data[league].add(event_id)
+                continue
+            
+            event_status = comp.get('status', {}).get('type', {}).get('id')
+            if event_status == '5' or event_status == '6':
+                continue
+            
+            event_date = comp.get('date')
+
+            season_info = header_info.get('season')
+            if not season_info:
+                incomplete_event_data[league].add(event_id)
+                continue
+            season_year = season_info.get('year')
+            season_type = season_info.get('type')
+            week = header_info.get('week')
+
+            teams = comp.get('competitors', [])
+            home = next((t for t in teams if t.get('homeAway') == 'home'), None)
+            away = next((t for t in teams if t.get('homeAway') == 'away'), None)
+            if not (home and away):
+                incomplete_event_data[league].add(event_id)
+                continue
+            home_team = home.get('team', {}).get('displayName')
+            away_team = away.get('team', {}).get('displayName')
+            home_rank = home.get('rank')
+            away_rank = away.get('rank')
+            ranked_matchup = bool(home_rank and away_rank)
+            one_ranked = bool(home_rank) != bool(away_rank)
+            home_winner = home.get('winner', False)
+            away_winner = away.get('winner', False)
+
+            neutral_site = comp.get('neutralSite', False)
+
+            win_probs = event_summary.get('winprobability', [])
+            pre_win_probs = next(iter(win_probs), {})
+            if not pre_win_probs:
+                incomplete_event_data[league].add(event_id)
+                continue
+            home_win_prob = pre_win_probs.get('homeWinPercentage') * 100
+            away_win_prob = 100 - home_win_prob
+
+            betting_data = next(iter(event_summary.get('pickcenter', [])), {})
+            home_american_ml = betting_data.get('homeTeamOdds', {}).get('moneyLine')
+            away_american_ml = betting_data.get('awayTeamOdds', {}).get('moneyLine')
+            home_decimal_ml = convert_to_decimal(home_american_ml)
+            away_decimal_ml = convert_to_decimal(away_american_ml)
+
+            print(f'League: {league}')
+            print(f'ESPN ID: {event_id}')
+            print(f'Date: {event_date}')
+            print(f'Season Year: {season_year}')
+            print(f'Week Number: {week}')
+            print(f'Season Type: {season_type}')
+            print(f'Home Team: {home_team}')
+            print(f'Home Rank: {home_rank}')
+            print(f'Home Win %: {home_win_prob}')
+            print(f'Home American ML: {home_american_ml}')
+            print(f'Home Decimal ML: {home_decimal_ml}')
+            print(f'Home Win: {home_winner}')
+            print(f'Away Team: {away_team}')
+            print(f'Away Rank: {away_rank}')
+            print(f'Away Win %: {away_win_prob}')
+            print(f'Away American ML: {away_american_ml}')
+            print(f'Away Decimal ML: {away_decimal_ml}')
+            print(f'Away Win: {away_winner}')
+            print(f'Neutral Site: {neutral_site}')
+            print(f'Both Ranked: {ranked_matchup}')
+            print(f'One Ranked: {one_ranked}')
+            print('\n\n')
     
     def request_with_retry(self, url, params):
         """ 
@@ -47,7 +134,7 @@ class Command(BaseCommand):
 
         for attempt in range(max_retries):
             try:
-                resp = requests.get(url, headers=headers, timeout=30)
+                resp = requests.get(url, headers=headers, params=params, timeout=30)
                 resp.raise_for_status()
                 return resp
             except Exception as e:
