df6ab09ddd8708a706db4ed53f4bcce1e3b69872
amalozzi300 <andrewmalozzi900@gmail.com>
2025-09-18 15:30:30 -0400

    added season_type field to Event model

--- espndata/espndata/eventdata/models.py
+++ espndata/espndata/eventdata/models.py
@@ -8,12 +8,17 @@ class Event(models.Model):
         ('nba', 'NBA'),
         ('mlb', 'MLB'),
     ]
+    SEASON_TYPE_CHOICES = [
+        (2, 'Regular Season'),
+        (3, 'Post Season'),
+    ]
 
     league = models.CharField(max_length=16, choices=LEAGUE_CHOICES)
     espn_id = models.CharField(max_length=24)
     date = models.DateField()
     season = models.IntegerField()
     week = models.IntegerField(null=True)
+    season_type = models.IntegerField(choices=SEASON_TYPE_CHOICES)
     home_team = models.CharField(max_length=128)
     home_rank = models.IntegerField(null=True)
     home_win_probability = models.DecimalField(max_digits=5, decimal_places=2)
