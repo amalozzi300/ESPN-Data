fa2fa55b23adf80cb6d4cf964901a25bae026b2e
amalozzi300 <andrewmalozzi900@gmail.com>
2025-10-14 00:46:34 -0400

    started collection state model

--- espndata/espndata/core/models.py
+++ espndata/espndata/core/models.py
@@ -1,3 +1,6 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class ProbabilityDataCollectionState(models.Model):
+    league = models.CharField(max_length=16, choices=settings.LEAGUE_CHOICES)
+    
