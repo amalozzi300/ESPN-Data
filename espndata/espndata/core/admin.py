60bda0776cc993963103550a5a363e542c7937de
amalozzi300 <andrewmalozzi900@gmail.com>
2025-10-29 21:44:42 -0400

    DataCollectionState model, initial migration, registered on admin site

--- espndata/espndata/core/admin.py
+++ espndata/espndata/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from espndata.core.models import DataCollectionState
+
+admin.site.register(DataCollectionState)
--- espndata/espndata/core/models.py
+++ espndata/espndata/core/models.py
@@ -1,6 +1,12 @@
 from django.conf import settings
 from django.db import models
 
-class ProbabilityDataCollectionState(models.Model):
+class DataCollectionState(models.Model):
     league = models.CharField(max_length=16, choices=settings.LEAGUE_CHOICES)
-    
+    collected = models.DateField()
+    season_start = models.DateField()
+    season_end = models.DateField()
+    all_star_start = models.DateField()
+    all_star_end = models.DateField()
+    season_type = models.IntegerField(choices=settings.SEASON_TYPE_CHOICES)
+    week = models.IntegerField()
