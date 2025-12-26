f70849354d5b50feeee327290c26f656a367836b
amalozzi300 <andrewmalozzi900@gmail.com>
2025-12-26 01:25:25 -0500

    registered LeagueDetails model admin page

--- espndata/espndata/core/admin.py
+++ espndata/espndata/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from espndata.core.models import DataCollectionState
+from espndata.core.models import DataCollectionState, LeagueDetails
 
 admin.site.register(DataCollectionState)
+admin.site.register(LeagueDetails)
