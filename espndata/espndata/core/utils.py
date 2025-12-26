d64fdc37b23c2325e0913c0279d0101130175391
amalozzi300 <andrewmalozzi900@gmail.com>
2025-12-26 14:58:57 -0500

    improved american_to_decimal logic

--- espndata/espndata/core/utils.py
+++ espndata/espndata/core/utils.py
@@ -1,13 +1,12 @@
 def american_to_decimal(american):
     """ 
     Converts American formatted betting odds to decimal formatted betting odds.
+    Implicitly returns None is `american` is None (or falsy).
     """
-    if american is None:
-        return american
-    
-    american = int(american)
+    if american:
+        american = int(american)
 
-    if american > 0:
-        return (american / 100) + 1
-    elif american < 0:
-        return 1 - (100 / american)
+        if american > 0:
+            return (american / 100) + 1
+        elif american < 0:
+            return 1 - (100 / american)
