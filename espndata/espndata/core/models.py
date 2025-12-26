55cdb170b2deae0b8227c59d7e65843cc11f9bcf
amalozzi300 <andrewmalozzi900@gmail.com>
2025-12-26 15:30:41 -0500

    DCS collected -> collection_date, added event_date

--- espndata/espndata/core/models.py
+++ espndata/espndata/core/models.py
@@ -30,12 +30,13 @@ class LeagueDetails(models.Model):
 
 class DataCollectionState(models.Model):
     league_details = models.OneToOneField(LeagueDetails, on_delete=models.CASCADE, related_name='data_collection_state')
-    collected = models.DateField()
+    collection_date = models.DateField()
     season_start = models.DateField()
     season_end = models.DateField()
     is_offseason = models.BooleanField()
     all_star_start = models.DateField(null=True, blank=True)
     all_star_end = models.DateField(null=True, blank=True)
+    event_date = models.DateField(null=True, blank=True)
     season_type = models.IntegerField(choices=settings.SEASON_TYPE_CHOICES)
     week = models.IntegerField(null=True, blank=True)
 
