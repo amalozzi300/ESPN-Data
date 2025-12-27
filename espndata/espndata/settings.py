6e5a99a3cf80b6255c94aecfe6f6544d575a0d81
amalozzi300 <andrewmalozzi900@gmail.com>
2025-12-27 01:10:24 -0500

    temporarily removed eventdata app from installed

--- espndata/espndata/settings.py
+++ espndata/espndata/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # custom applications
     'espndata.events.apps.EventsConfig',
 
-    'espndata.eventdata.apps.EventdataConfig',
+    # 'espndata.eventdata.apps.EventdataConfig',
     'espndata.core.apps.CoreConfig',
 ]
 
