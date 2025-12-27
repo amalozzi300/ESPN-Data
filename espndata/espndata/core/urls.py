b2251213fa0ac949da4e9dd6d556c2b0952490b5
amalozzi300 <andrewmalozzi900@gmail.com>
2025-12-27 01:29:15 -0500

    login/logout views, login template

--- espndata/espndata/core/urls.py
+++ espndata/espndata/core/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from espndata.core import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
 ]
--- espndata/espndata/core/views.py
+++ espndata/espndata/core/views.py
@@ -1,5 +1,17 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'core/home.html'
+
+class Login(LoginView):
+    template_name = 'core/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse('home')
+
+class Logout(LogoutView):
+    next_page = reverse_lazy('home')
