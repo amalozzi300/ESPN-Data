8927c9ee6c11278bfaba7dac12ede71ac3197493
amalozzi300 <andrewmalozzi900@gmail.com>
2026-01-03 15:25:12 -0500

    modified login, logout, register naming convention

--- espndata/espndata/core/urls.py
+++ espndata/espndata/core/urls.py
@@ -5,7 +5,7 @@ from espndata.core import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
 
-    path('login/', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
-    path('register', views.Register.as_view(), name='register'),
+    path('login/', views.LoginUser.as_view(), name='login_user'),
+    path('logout/', views.LogoutUser.as_view(), name='logout_user'),
+    path('register', views.RegisterUser.as_view(), name='register_user'),
 ]
--- espndata/espndata/core/views.py
+++ espndata/espndata/core/views.py
@@ -9,19 +9,19 @@ from django.views.generic import CreateView, TemplateView
 class Home(TemplateView):
     template_name = 'core/home.html'
 
-class Login(LoginView):
-    template_name = 'core/login.html'
+class LoginUser(LoginView):
+    template_name = 'core/login_user.html'
     redirect_authenticated_user = True
 
     def get_success_url(self):
         return reverse('home')
 
-class Logout(LogoutView):
+class LogoutUser(LogoutView):
     next_page = reverse_lazy('home')
 
-class Register(CreateView):
+class RegisterUser(CreateView):
     form_class = UserCreationForm
-    template_name = 'core/register.html'
+    template_name = 'core/register_user.html'
     success_url = reverse_lazy('home')
 
     def form_valid(self, form):
