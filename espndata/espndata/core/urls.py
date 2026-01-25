a701014663e61a6ddaa007082da75068f1a5b6f2
amalozzi300 <andrewmalozzi900@gmail.com>
2026-01-25 03:26:45 -0500

    fixed temp homepage issue

--- espndata/espndata/core/urls.py
+++ espndata/espndata/core/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from espndata.core import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
+    # path('', views.Home.as_view(), name='home'),
 
-    path('login/', views.LoginUser.as_view(), name='login_user'),
-    path('logout/', views.LogoutUser.as_view(), name='logout_user'),
-    path('register', views.RegisterUser.as_view(), name='register_user'),
+    # path('login/', views.LoginUser.as_view(), name='login_user'),
+    # path('logout/', views.LogoutUser.as_view(), name='logout_user'),
+    # path('register', views.RegisterUser.as_view(), name='register_user'),
 ]
